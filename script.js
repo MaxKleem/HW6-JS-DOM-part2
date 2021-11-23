9c1c83e7b124a847fb6e071a7f2d809f74f1c0e0
MaxKleem <maxkleem@ukr.net>
2021-11-23 14:26:06 +0200

    HW6-JS-DOM-part2
    HW6

---
+++ script.js
@@ -0,0 +1,11 @@
+let inp = document.querySelector('input');
+let btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  let result = inp.value;
+  document.querySelector('div').style.background = result;
+});
+let select = document.querySelector('#selectedElement');
+const figure = document.querySelector('#wrapper');
+select.addEventListener('change', (event) => {
+  figure.className = event.target.value;
+})
